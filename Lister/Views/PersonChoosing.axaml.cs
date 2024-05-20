72c38acba407dd7fd2ff26662a65fcb31559310f
MML <mml248@yandex.ru>
2024-05-20 08:35:12 +0700

    Revert "wrong"
    This reverts commit c3916161273dbab36cb51f5d26f63cdd17267576.

---
+++ Lister/Views/PersonChoosing.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Lister.Views
+{
+    public partial class PersonChoosing : UserControl
+    {
+        public PersonChoosing ( )
+        {
+            InitializeComponent ( );
+        }
+    }
+}
