c7f046b0aa7e51fc144170cad302c3230319ac0f
MML <mml248@yandex.ru>
2025-02-24 10:44:46 +0700

    Собрал Core

---
+++ Core/DataAccess/PeopleSource/Abstractions/PeopleSourceBase.cs
@@ -0,0 +1,8 @@
+﻿using Core.Models;
+
+namespace Core.DataAccess.PeopleSource.Abstractions;
+
+public abstract class PeopleSourceBase
+{
+    public abstract List<Person>? Get ( string? personsFilePath, int gettingLimit );
+}
---
+++ Core/Models/Badge/Badge.cs
@@ -0,0 +1,36 @@
+﻿namespace Core.Models.Badge
+{
+    public class Badge
+    {
+        public Person Person { get; private set; }
+        public string BackgroundImagePath { get; private set; }
+        public Layout BadgeLayout { get; private set; }
+
+
+        private Badge ( Person person, string backgroundImagePath, Layout layout )
+        {
+            Person = person;
+            BackgroundImagePath = backgroundImagePath;
+            BadgeLayout = layout;
+
+            Dictionary<string, string> personProperties = Person.GetProperties ();
+
+            BadgeLayout.SetTextualValues ( personProperties );
+            BadgeLayout.TrimEdgeCharsOfAtomContent ();
+        }
+
+
+        public static Badge? GetBadge ( Person person, string backgroundImagePath, Layout layout )
+        {
+            bool isArgumentNull = ( person == null ) || ( backgroundImagePath == null ) || ( layout == null );
+
+            if ( isArgumentNull )
+            {
+                return null;
+            }
+
+            return new Badge ( person, backgroundImagePath, layout );
+        }
+    }
+
+}
